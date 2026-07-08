15f913d6e3c16edfa137cc4f44e8152ac7d99349
Nassimovic <nassim.saa@hotmail.fr>
2026-07-08 11:33:53 +0200

    feat: implementing the ag grid component and trying it on a few ships

--- src/app/features/starship-grid/starship-grid.ts
+++ src/app/features/starship-grid/starship-grid.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
+import { AllCommunityModule, ColDef, ModuleRegistry } from 'ag-grid-community';
 
 import { Starship, SwapiService } from '../../core/services/swapi.service';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 @Component({
   selector: 'app-starship-grid',
@@ -12,12 +15,19 @@ import { Starship, SwapiService } from '../../core/services/swapi.service';
 export class StarshipGridComponent implements OnInit {
   private readonly swapiService = inject(SwapiService);
 
-  starships: Starship[] = [];
-  
+  starships = signal<Starship[]>([]);
+
+  colDefs: ColDef[] = [
+    { field: 'name', headerName: 'Name', resizable: true },
+    { field: 'model', headerName: 'Model', resizable: true },
+    { field: 'manufacturer', headerName: 'Manufacturer', resizable: true },
+    { field: 'crew', headerName: 'Crew', resizable: true },
+    { field: 'passengers', headerName: 'Passengers', resizable: true },
+  ];
 
   ngOnInit(): void {
     this.swapiService.getStarships().subscribe((response) => {
-      this.starships = response.results;
+      this.starships.set(response.results);
     });
   }
 }
